b6f33260bae67abda9d3e45afb309c6ddcf6e170
DanielEmidio1988 <daniel.emidio.silva@gmail.com>
2022-09-14 16:01:08 -0300

    Entrega do Exercicio 14/09

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -4,31 +4,31 @@
 // a) Uma função que receba 2 números como parâmetros, e, dentro da função, faça a soma das duas entradas e imprima o resultado. 
 // Invoque a função e imprima no console o resultado.
 
-// function calculaDoisNum (n1,n2){
-//     console.log(`${n1} + ${n2} = ${n1 + n2}`)
-// }
+function calculaDoisNum (n1,n2){
+    console.log(`${n1} + ${n2} = ${n1 + n2}`)
+}
 
-// calculaDoisNum(Number(prompt("Informe o primeiro numero")), Number(prompt("Informe o segundo numero")))
+calculaDoisNum(Number(prompt("Informe o primeiro numero")), Number(prompt("Informe o segundo numero")))
 
 // b) Uma função que recebe 2 números e imprime um booleano que informa se o primeiro número é **maior ou igual** ao segundo.
 
-// function validaNumero (n1,n2){
-//     console.log(`${n1} é maior que ${n2}? ${n1>=n2}`)
-// }
+function validaNumero (n1,n2){
+    console.log(`${n1} é maior que ${n2}? ${n1>=n2}`)
+}
 
-// validaNumero(Number(prompt("Informe o primeiro numero")), Number(prompt("Informe o segundo numero")))
+validaNumero(Number(prompt("Informe o primeiro numero")), Number(prompt("Informe o segundo numero")))
 
 // c) Uma função que receba um número e imprima se ele é par ou não
 
-// function verificaPar (n1){
-//     if(n1%2 === 0){
-//         console.log(`${n1} é um numero par`)
-//     }else{
-//         console.log(`${n1} é um numero impar`)
-//     }
-// }
+function verificaPar (n1){
+    if(n1%2 === 0){
+        console.log(`${n1} é um numero par`)
+    }else{
+        console.log(`${n1} é um numero impar`)
+    }
+}
 
-// verificaPar(Number(prompt("Informe um numero")))
+verificaPar(Number(prompt("Informe um numero")))
 
 // d) Uma função que recebe uma mensagem (`string`) como parâmetro e imprima o tamanho dessa mensagem, 
 // juntamente com uma versão dela contendo apenas letras maiúsculas.
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -3,24 +3,24 @@
 // Crie 4 funções, cada uma deve receber dois parâmetros numéricos e retornar o valor uma das operações 
 // básicas (soma, subtração, multiplicação e divisão).
 
-// function soma(n1,n2){
-//     console.log(`${n1} + ${n2} = ${n1+n2}`)
-// }
+function soma(n1,n2){
+    console.log(`${n1} + ${n2} = ${n1+n2}`)
+}
 
-// function substracao(n1,n2){
-//     console.log(`${n1} - ${n2} = ${n1-n2}`)
-// }
-// function multiplicacao(n1,n2){
-//     console.log(`${n1} x ${n2} = ${n1*n2}`)
-// }
-// function divisao(n1,n2){
-//     console.log(`${n1} / ${n2} = ${n1/n2}`)
-// }
+function substracao(n1,n2){
+    console.log(`${n1} - ${n2} = ${n1-n2}`)
+}
+function multiplicacao(n1,n2){
+    console.log(`${n1} x ${n2} = ${n1*n2}`)
+}
+function divisao(n1,n2){
+    console.log(`${n1} / ${n2} = ${n1/n2}`)
+}
 
-// soma(4,2)
-// substracao(2,2)
-// multiplicacao(3,2)
-// divisao(8,2)
+soma(4,2)
+substracao(2,2)
+multiplicacao(3,2)
+divisao(8,2)
 
 // Em seguida, peça para o usuário inserir dois números e chame essas 4 funções com esses valores inputados pelo 
 // usuário sendo o argumento. Por fim, imprima no console o resultado das operações
